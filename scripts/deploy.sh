eb636e1cf9c76507d0489dd263b8b72af623f8e2
Harry P <hpanay@gmail.com>
2020-03-03 22:15:09 +0000

    Add submodule commit step and cleanup script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,18 +8,26 @@ log() {
   printf "\033[0;32m$(date) - $1...\033[0m\n"
 }
 
-log "Deploying to github"
+log "Staging Github Pages static content"
 
-# Go To Public folder
 cd public
-
-# Add changes to git
 git add .
 
-# Commit changes
-git commit -m "$(date) - Pushing to github"
+log "Comitting Github Pages content"
 
-# Push source and build repos.
+git commit -m "$(date) - Pushing to Github Pages"
 git push origin master
 
+log "Pushed to Github Pages"
+log "Staging submodule commit update"
+
+cd ..
+git add ./public
+
+log "Comitting submodule update"
+
+git commit -m "$(date) - Updating submodule"
+git push origin master
+
+log "Pushed submodule commit update"
 log "All done"
